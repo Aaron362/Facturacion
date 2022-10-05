45092c8854de211e0fa4ed41c97f0367316786da
Aaron <daaroncg135@gmail.com>
2022-10-05 16:23:54 -0500

    Cambios del documento 1
    Se añadieron los cambios que se encuentran en el  documento 1

---
+++ facturacion/Cliente.java
@@ -0,0 +1,17 @@
+package com.tuempresa.facturacion.modelo;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+public class Cliente {
+@Id
+
+@Column(length=6)
+int numero;
+
+@Column(length=50)
+String nombre;
+}
