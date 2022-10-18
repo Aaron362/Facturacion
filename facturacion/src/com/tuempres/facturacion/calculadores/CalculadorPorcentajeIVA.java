29ed17b5a06871c3ccda3553c161726ebb2760c4
Aaron <daaroncg135@gmail.com>
2022-10-18 16:25:00 -0500

    Commit 13

---
+++ facturacion/src/com/tuempres/facturacion/calculadores/CalculadorPorcentajeIVA.java
@@ -0,0 +1,12 @@
+package com.tuempres.facturacion.calculadores;
+
+import org.openxava.calculators.*;
+
+import com.tuempresa.facturacion.util.*;
+
+public class CalculadorPorcentajeIVA implements ICalculator {
+  @Override
+  public Object calculate() throws Exception	{
+	  return PreferenciasFacturacion.getPorcentajeIVADefecto();
+  }
+}
